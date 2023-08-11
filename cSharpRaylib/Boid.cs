673c0deba52464b2ad60d95aa5a165c336a1293c
michal <michal@DESKTOP-CEL5COG>
2023-08-11 10:25:06 +0200

    Add project files.

---
+++ cSharpRaylib/Boid.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Raylib_cs;
+
+namespace cSharpRaylib
+{
+    internal class Boid
+    {
+        private Vector2 windowSize;
+
+        private float maxSpeed = 75;
+        private float minSpeed = 25;
+
+        private float separationDistance = 30;
+        private float separationForce = 5f;
+
+        private float alignmentDistance = 30;
+        private float alignmentForce = 3f;
+
+        private float cohesionDistance = 20;
+        private float cohesionForce = 2f;
+
+        private Vector2 position;
+        private Vector2 velocity;
+        private Vector2 acceleration = new Vector2(0, 0);
+
+        private Stopwatch timer = new Stopwatch();
+
+        public Boid(Vector2 windowSize, Vector2 position, Vector2 velocity)
+        {
+            this.windowSize = windowSize;
+
+            this.position = position;
+            this.velocity = velocity;
+        }
+
+        private void Separation(List<Boid> flockmates)
+        {
+            foreach (Boid bo in flockmates)
+            {
+                // Skip yourself
+                if (bo == this) continue;
+
+                // Skip far away flockmates
+                var d = (separationDistance - Vector2.Distance(this.position, bo.position))/separationDistance;
+                if (d < 0) continue;
+
+                var norm = Vector2.Normalize(position - bo.position);
+
+                acceleration += norm * d * separationForce;
+            }
+        }
+
+        private void Alignment(List<Boid> flockmates)
+        {
+            foreach (Boid bo in flockmates)
+            {
+                // Skip yourself
+                if (bo == this) continue;
+
+                // Skip far away flockmates
+                var d = (alignmentDistance - Vector2.Distance(this.position, bo.position)) / alignmentDistance;
+                if (d < 0) continue;
+
+                var normBo = Vector2.Normalize(bo.velocity);
+                var norm = Vector2.Normalize(velocity);
+
+                var af = normBo - norm;
+
+                acceleration += af * d * alignmentForce; 
+            }
+        }
+
+        private void Cohesion(List<Boid> flockmates)
+        {
+            Vector2 avrgPos = new Vector2(0);
+            int count = 0;
+
+            foreach (Boid bo in flockmates)
+            {
+                // Skip yourself
+                if (bo == this) continue;
+
+                // Skip far away flockmates
+                var d = (cohesionDistance - Vector2.Distance(this.position, bo.position)) / cohesionDistance;
+                if (d < 0) continue;
+
+                avrgPos += bo.position;
+                count ++;
+            }
+
+            if (count == 0) return;
+
+            avrgPos /= count;
+
+            acceleration += (avrgPos - position) * cohesionForce;
+        }
+
+        public void Update(List<Boid> flockmates)
+        {
+            float dt = (float)timer.Elapsed.TotalSeconds;
+
+            Separation(flockmates);
+            Alignment(flockmates);
+            Cohesion(flockmates);
+
+            velocity += acceleration * dt;
+
+            velocity = Vector2.Clamp(velocity, new Vector2(-maxSpeed), new Vector2(maxSpeed));
+            if (velocity.Length() < minSpeed)
+            {
+                velocity = Vector2.Normalize(velocity)*minSpeed;
+            }
+
+            position += velocity * dt;
+            acceleration *= 0;
+
+            if (position.X >= windowSize.X)
+                position.X -= windowSize.X;
+            if (position.Y >= windowSize.Y)
+                position.Y -= windowSize.Y;
+            if (position.X < 0)
+                position.X += windowSize.X;
+            if (position.Y < 0)
+                position.Y += windowSize.Y;
+
+            timer.Restart();
+        }
+
+        public void Draw()
+        {
+            var normal = new Vector2(0, 1);
+            var angle = MathF.Atan2(velocity.Y, velocity.X) + MathF.PI/2;
+
+            var back = new Vector2(0, 15);
+            var backL = Vector2.Transform(back, Matrix3x2.CreateRotation(angle - 0.2f));
+            var backR = Vector2.Transform(back, Matrix3x2.CreateRotation(angle + 0.2f));
+            Raylib.DrawTriangle(position, backR+position, backL+position, Color.BLACK);
+        }
+    }
+}
---
+++ cSharpRaylib/Program.cs
@@ -0,0 +1,62 @@
+﻿using Raylib_cs;
+using System.Numerics;
+using System.Security.Cryptography;
+
+namespace cSharpRaylib
+{
+    static class Program
+    {
+        private static float boidSpeed = 10;
+        private static int boidCount = 500;
+
+        private static List<Boid> boids = new List<Boid>();
+        private static Vector2 windowSize = new Vector2(800, 480);
+
+        public static void Main(string[] args)
+        {
+            initBoids(boidCount);
+
+            Raylib.InitWindow((int)windowSize.X, (int)windowSize.Y, "Hello World");
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.WHITE);
+
+                drawBoids();
+
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
+        }
+
+        private static void initBoids(int boidCount)
+        {
+            var rnd = new Random();
+
+            for (int i = 0; i < boidCount; i++)
+            {
+                boids.Add(new Boid(
+                    windowSize, 
+                    new Vector2(
+                        (float)rnd.NextDouble()*windowSize.X,
+                        (float)rnd.NextDouble() * windowSize.Y
+                    ), 
+                    new Vector2(
+                        (float)(rnd.NextDouble()*2 - 1) * boidSpeed,
+                        (float)(rnd.NextDouble()*2 - 1) * boidSpeed
+                    )));
+            }
+        }
+
+        private static void drawBoids()
+        {
+            foreach (var boid in boids)
+            {
+                boid.Update(boids);
+                boid.Draw();
+            }
+        }
+    }
+}
